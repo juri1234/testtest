65bc6fc331a0d0f6cfc25f6f5597c2edffff708e
dlrkd <dlrkd@10.10.16.190>
2023-06-22 21:20:29 +0900

    mapper commit

--- demo/src/main/java/com/example/demo/mapper/DemoMapper.java
+++ demo/src/main/java/com/example/demo/mapper/DemoMapper.java
@@ -1,8 +1,10 @@
 package com.example.demo.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper
 public interface DemoMapper {
 
+	public String mapper();
 }
